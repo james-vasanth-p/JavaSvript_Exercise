ac503bdab6e196cb6d5ac080279637de655224da
JAMES <94291809+james-vasanth-p@users.noreply.github.com>
2025-08-11 22:45:36 +0530

    Update index.js

--- L08 - Functions/index.js
+++ L08 - Functions/index.js
@@ -10,7 +10,11 @@
 // Function Parameters and Arguments
 /*Functions can take parameters, which act as placeholders for the values 
 that will be passed to the function.The passing value is called an argument.*/
+function printer(name = 'jade'){
+ console.log(`Hi, ${name}`. Do you want 2 copies` );
+ }
 
+ function(james);
 // Default Parameter
 
 // Function with Return Type
