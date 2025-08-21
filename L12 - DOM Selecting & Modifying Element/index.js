f8812f1e99bb18ea12ba8fc0a389bfb7e4f51603
james-vasanth-p <jamesvasanth200501@gmail.com>
2025-08-21 22:17:03 +0530

    Dom exercise

--- L12 - DOM Selecting & Modifying Element/index.js
+++ L12 - DOM Selecting & Modifying Element/index.js
@@ -7,3 +7,21 @@ The following the way to Selecting & Modifying Element in DOM
 4.querySelector
 5.querySelectorAll
 */
+const idname = document.getElementById('main-heading')
+console.log(idname.innerText)
+dommanipulation();
+function dommanipulation(){
+    setTimeout(()=>{
+       idname.innerText = 'james is learning'
+    },5000)
+}
+
+const classdom = document.getElementsByClassName('list-item')
+console.log(classdom[1])
+classdom[2].innerText = 'sayHi';
+
+const tagdom = document.getElementsByTagName('p')
+console.log(tagdom[0].innerText)
+
+const tagarray = Array.tagdom
+//console.log(tagarray)
