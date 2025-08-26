0590821bd8d0b3fb1197c047744a1c195a72795a
james-vasanth-p <jamesvasanth200501@gmail.com>
2025-08-26 07:44:16 +0530

    EH-3

--- L15 - Event Handing Part 3/index.js
+++ L15 - Event Handing Part 3/index.js
@@ -1,16 +1,18 @@
 const feedbackForm = document.getElementById('feedbackForm');
 const response = document.getElementById('response');
 
-
-feedbackForm.addEventListener('submit',function (event) {
+feedbackForm.addEventListener('submit', function (event) {
   event.preventDefault();
   const formData = new FormData(this);
 
-  console.log(formData);
-  
-  const jsonobj = JSON.stringify(formData);
+  // Convert FormData to a plain object
+  const obj = {};
+  formData.forEach((value, key) => {
+    obj[key] = value;
+  });
+
+  const jsonobj = JSON.stringify(obj,null,2);
   console.log(jsonobj);
-  response.textcontent = jsonobj;
 
-
-})
+  response.innerHTML = `<pre>${jsonobj}</pre>`; // Fixed typo: textContent (not
+});
