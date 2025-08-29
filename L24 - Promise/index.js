682dcb74703c5f3ed3f757c9d3a4ad8cfced01fb
james-vasanth-p <jamesvasanth200501@gmail.com>
2025-08-29 07:35:10 +0530

    promise

---
+++ L24 - Promise/index.js
@@ -0,0 +1,19 @@
+// Example: Simple Promise that resolves after 2 seconds
+
+function asyncTask() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Task completed!');
+        }, 2000);
+    },error=>{
+        alert(error);
+    });
+}
+
+asyncTask()
+    .then(result => {
+        console.log(result); // Output: Task completed!
+    })
+    .catch(error => {
+        console.error(error);
+    });
